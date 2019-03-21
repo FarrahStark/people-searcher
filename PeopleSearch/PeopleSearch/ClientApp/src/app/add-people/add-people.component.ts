b28999a3c0d295cddcf2917089c37c308f0ae064
Paul Miller <paulmiller488@gmail.com>
2019-03-21 00:16:33 -0600

    hid the add people functionality due to time constraints

--- PeopleSearch/PeopleSearch/ClientApp/src/app/add-people/add-people.component.ts
+++ PeopleSearch/PeopleSearch/ClientApp/src/app/add-people/add-people.component.ts
@@ -6,18 +6,6 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './add-people.component.html'
 })
 export class AddPeopleComponent {
-  public forecasts: WeatherForecast[];
-
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/Person/Search').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
   }
-}
-
-interface WeatherForecast {
-  dateFormatted: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
 }
